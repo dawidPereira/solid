239e90246b7ffe52a390984236eec5c846a541bc
Dawid Pereira <pereiradawid@outlook.com>
2019-09-07 00:43:46 +0200

    Dodaj pliki projektów.

---
+++ Single Responsibility Principle/After/After.cs
@@ -0,0 +1,41 @@
+﻿namespace solid.Single_Responsibility_Principle.After
+{
+   public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+        public void SetName(string name)
+        {
+            //...
+        }
+    }
+
+    public class EmailValidator
+    {
+        public bool ValidateEmail(string email)
+        {
+            //...
+            return default;
+        }
+    }
+
+    public class UserRepository
+    {
+        public void SaveToDb()
+        {
+            //...
+        }
+    }
+
+    public class PrinterManager
+    {
+        public void Print()
+        {
+            //...
+        }
+    }
+}
---
+++ Single Responsibility Principle/Before/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solid.Single_Responsibility_Principle.Before
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+        public void SetName(string name)
+        {
+            //...
+        }
+        public bool ValidateEmail(string email)
+        {
+            //...
+            return default;
+        }
+
+        public void SaveToDb()
+        {
+            //...
+        }
+
+        public void PrintUserData()
+        {
+            //...
+        }
+    }
+}
