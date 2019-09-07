0bec958fb843fc5f7105cad5f4db7d7ec2f4ceaf
Dawid Pereira <pereiradawid@outlook.com>
2019-09-07 14:22:42 +0200

    Open Closed Principle

---
+++ Open Closed Principle/After/SecurityGuard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace solid.Open_Closed_Principle.After
+{
+    public class SecurityGuard
+    {
+        private List<ISpecification<User>> _specifications;
+
+        public bool ValidateUser(User user)
+        {
+            return _specifications.Select(x => x.IsSatisfiedBy(user))
+                .All(x => x);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Cloth { get; set; }
+        public string Age { get; set; }
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T candidate);
+    }
+
+    public class UserNameSpecification : ISpecification<User>
+    {
+        public bool IsSatisfiedBy(User user)
+        {
+            //...
+            return default;
+        }
+    }
+}
---
+++ Open Closed Principle/Before/SecurityGuard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace solid.Open_Closed_Principle.Before
+{
+    public class SecurityGuard
+    {
+        public bool ValidateUser(User user)
+        {
+            return ValidateName(user.Name)
+                    && ValidateCloth(user.Cloth)
+                    && ValidateAge(user.Age);
+        }
+
+        private bool ValidateName(string name)
+        {
+            //...
+            return default;
+        }
+        private bool ValidateCloth(string cloth)
+        {
+            //...
+            return default;
+        }
+        private bool ValidateAge(string age)
+        {
+            //...
+            return default;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Cloth { get; set; }
+        public string Age { get; set; }
+    }
+}
