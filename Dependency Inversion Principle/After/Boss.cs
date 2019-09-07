eb8e506220587679e9e29786b878c5ded371d422
Dawid Pereira <pereiradawid@outlook.com>
2019-09-07 19:00:03 +0200

    Dependency Inversion Principle

---
+++ Dependency Inversion Principle/After/Boss.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solid.Dependency_Inversion_Principle.After
+{
+    public class Boss
+    {
+        private readonly IITDepartment _itDepartment;
+
+        public Boss(IITDepartment itDepartment)
+        {
+            _itDepartment = itDepartment;
+        }
+
+        public void Work(Computer computer)
+        {
+            if (computer.IsBroke())
+            {
+                _itDepartment.FixComputer(computer);
+            }
+        }
+    }
+
+    public interface IITDepartment
+    {
+        void FixComputer(Computer computer);
+    }
+
+    public class Computer
+    {
+        public bool IsBroke()
+        {
+            return default;
+        }
+    }
+}
---
+++ Dependency Inversion Principle/Before/Boss.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solid.Dependency_Inversion_Principle.Before
+{
+    public class Boss
+    {
+        public void Work(Computer computer)
+        {
+            if (computer.IsBroke())
+            {
+                FixComputer(computer);
+            }
+        }
+
+        private void FixComputer(Computer computer)
+        {
+
+        }
+    }
+
+    public class Computer
+    {
+        public bool IsBroke()
+        {
+            return default;
+        }
+    }
+}
